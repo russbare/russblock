6d6c3a141999d70b8abbc4695de1a8080bee12f2
russ voisan <russ.voisan@gmail.com>
2019-05-15 13:04:06 -0700

    Update card.js

---
+++ src/blocks/card.js
@@ -0,0 +1,13 @@
+export const card = {
+  title: 'Card',
+  icon: 'format-image',
+  category: 'russblock',
+  supports: {},
+  attributes: {},
+  edit(props) {
+    return();
+  },
+  save(props) {
+    return();
+  }
+}
