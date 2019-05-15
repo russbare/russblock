afd50dadf1009dc1791e1487de6602689a8d3f4d
russ voisan <russ.voisan@gmail.com>
2019-05-15 13:03:46 -0700

    Update columns.js

---
+++ src/blocks/columns.js
@@ -0,0 +1,13 @@
+export const columns = {
+  title: 'Columns',
+  icon: 'format-image',
+  category: 'russblock',
+  supports: {},
+  attributes: {},
+  edit(props) {
+    return();
+  },
+  save(props) {
+    return();
+  }
+}
