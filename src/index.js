df588cf4b226deecbfce29998138d3c35fb169be
russ voisan <russ.voisan@gmail.com>
2019-05-15 13:03:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const { registerBlockType } = wp.blocks;
 
 import { hero } from './blocks/hero';
+import { section } from './blocks/section';
+import { accordion } from './blocks/accordion';
+import { accordionItem } from './blocks/accordion-item';
 
 registerBlockType('russblock/hero', hero);
+registerBlockType('russblock/section', section);
+registerBlockType('russblock/accordion', accordion);
+registerBlockType('russblock/accordionItem', accordionItem);
